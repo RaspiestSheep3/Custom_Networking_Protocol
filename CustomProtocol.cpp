72a11025416cea444b44d66cadcfd699d3b063b9
RaspiestSheep3 <raspiestsheep3@hotmail.com>
2026-05-28 15:28:58 +0100

    Initial Commit w/ Writeup

---
+++ CustomProtocol.cpp
@@ -0,0 +1,12 @@
+﻿// CustomProtocol.cpp : Defines the entry point for the application.
+//
+
+#include "CustomProtocol.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
